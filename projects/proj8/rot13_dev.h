a6ba813310877ecc6c60d7d34f4125f8a077030a
Andrew Wiles <acw3f@pianosa.org>
2015-04-16 12:51:56 -0700

    Added Project 8

---
+++ projects/proj8/rot13_dev.h
@@ -0,0 +1,15 @@
+/*
+ * rot13_dev.h
+ *
+ *  Created on: Apr 11, 2015
+ *      Author: acw3f
+ */
+
+#ifndef ROT13_DEV_H_
+#define ROT13_DEV_H_
+
+void *init_rot13_dev(void (*interrupt_fn)(void *data), void *data);
+
+
+
+#endif /* ROT13_DEV_H_ */
